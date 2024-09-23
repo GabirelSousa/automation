16aa561011c6a96b100963d35e69a5f25e2c3fa4
Gabriel Sousa <gasous4@gmail.com>
2024-09-23 15:56:26 -0400

    Adding data-test into elements

--- Cypress/app/forms/page.jsx
+++ Cypress/app/forms/page.jsx
@@ -37,16 +37,17 @@ export default function FormsPage(){
     const [subMessage, setSubMessage] = useState('')
     return (
         <main className={styles.main}>
-            <h1 className={styles.header}>Testing Forms</h1>
+            <h1 data-test='Forms header' className={styles.header}>Testing Forms</h1>
             <ItemsAccordion items={items} />
-            <TextField 
+            <TextField
+            data-test='email field' 
             className={styles.input} 
             label="Email" 
             variant="filled" 
             value={inputValue}
             onChange={e => setInputValue(e.target.value)}
             />
-            <Button onClick={() => {
+            <Button data-test='Button' onClick={() => {
                 if (!inputValue.includes('.com') ){
                     setSubMessage(`Invalid email: ${inputValue}!`)
                 } else if (inputValue.length){
