e3bf6b94e212f6dbdb029411fdd4b04aa3252658
Gabriel Sousa <gasous4@gmail.com>
2024-10-01 12:04:47 -0400

    fixtures json file

--- Cypress/cypress/e2e/modals.cy.js
+++ Cypress/cypress/e2e/modals.cy.js
@@ -17,19 +17,24 @@ describe('template spec', () => {
   })
 
   it.only('Test form modal', () => {
+    cy.fixture('data.json').as('userData')
     cy.get('#formModal').click()
     cy.get('#pum_popup_title_674').should('be.visible')
-    cy.get('#g1051-name').type('Gabriel')
-    cy.get('#g1051-email').type('gabriel@test.com')
-    cy.get('#contact-form-comment-g1051-message').type('This is a message')
+    cy.get('@userData').then((user) => {
+      cy.get('#g1051-name').type(user.name)
+      cy.get('#g1051-email').type(user.email)
+      cy.get('#contact-form-comment-g1051-message').type(user.message)
+    })
     cy.get('.pushbutton-wide').click()
     cy.get('#pum_popup_title_674').should('not.be.visible')
 
     cy.get('#formModal').click()
     cy.get('#pum_popup_title_674').should('be.visible')
-    cy.get('#g1051-name').type('Gabriel')
-    cy.get('#g1051-email').type('gabriel@test.com')
-    cy.get('#contact-form-comment-g1051-message').type('This is a message')
+    cy.get('@userData').then((user) => {
+      cy.get('#g1051-name').type(user.name)
+      cy.get('#g1051-email').type(user.email)
+      cy.get('#contact-form-comment-g1051-message').type(user.message)
+    })
     cy.get('[class="pum-close popmake-close"]').click({ multiple: true, force: true })
     cy.get('#pum_popup_title_674').should('not.be.visible')
   })
